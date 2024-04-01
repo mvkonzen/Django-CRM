bc421287308851198f609cd4905bc906f609c784
mvkonzen <marcuskonzen@gmail.com>
2024-04-01 17:36:48 -0300

    registration

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+
 
 
 
@@ -26,6 +28,23 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.changed_data['passwor1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Usuário registrado com sucesso!')
+            return redirect('home')
+        
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+
+
+
     return render(request, 'register.html', {})
 
 
